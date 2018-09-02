16a507336c92994cf1183b313c714bb2686af91f
robywas <wasilewski.tg@gmail.com>
2018-09-02 16:47:12 +0200

    Top Menu Buttons Resources bundles - internationalization

--- src/main/java/pl/moja/biblioteczka/Main.java
+++ src/main/java/pl/moja/biblioteczka/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -13,11 +16,15 @@ public class Main extends Application {
     }
     public void start(Stage primaryStage) throws Exception {
 
+ //       Locale.setDefault(new Locale("en"));
+
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml"));
  //       BorderPane borderPane = loader.load();
 //        Scene scene = new Scene(borderPane);
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
         primaryStage.setScene(new Scene((BorderPane)loader.load()));
-        primaryStage.setTitle("Moja Biblioteka");
+        primaryStage.setTitle(bundle.getString("title.application"));
         primaryStage.show();
     }
 }
--- src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
@@ -1,15 +1,28 @@
 package pl.moja.biblioteczka.controllers;
 
+import javafx.fxml.FXML;
+
 public class TopMenuButtonsController {
 
+    private MainController mainController;
 
+    @FXML
     public void openLibrary() {
+        System.out.println("openLibrary");
     }
 
+    @FXML
     public void openListBooks() {
+        System.out.println("openListBooks");
     }
 
+    @FXML
     public void openStatistics() {
+        System.out.println("openStatistics");
+    }
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
     }
 
 
