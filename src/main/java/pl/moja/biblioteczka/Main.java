443367ed2a65cead4fd2aba979c7c7bbdfb499f4
robywas <wasilewski.tg@gmail.com>
2018-09-03 10:57:58 +0200

    Top menu

--- src/main/java/pl/moja/biblioteczka/Main.java
+++ src/main/java/pl/moja/biblioteczka/Main.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Main extends Application {
@@ -14,16 +13,17 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     public void start(Stage primaryStage) throws Exception {
 
- //       Locale.setDefault(new Locale("en"));
+        //       Locale.setDefault(new Locale("en"));
 
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml"));
- //       BorderPane borderPane = loader.load();
-//        Scene scene = new Scene(borderPane);
         ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
         loader.setResources(bundle);
-        primaryStage.setScene(new Scene((BorderPane)loader.load()));
+        BorderPane borderPane = loader.load();
+        Scene scene = new Scene(borderPane);
+        primaryStage.setScene(scene);
         primaryStage.setTitle(bundle.getString("title.application"));
         primaryStage.show();
     }
--- src/main/java/pl/moja/biblioteczka/controllers/MainController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/MainController.java
@@ -1,7 +1,21 @@
 package pl.moja.biblioteczka.controllers;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+import static javafx.application.Application.STYLESHEET_CASPIAN;
+import static javafx.application.Application.STYLESHEET_MODENA;
 
 public class MainController {
 
@@ -11,8 +25,46 @@ public class MainController {
     @FXML
     private TopMenuButtonsController topMenuButtonsController;
 
-    private void initialize(){
+    @FXML
+    private void initialize() {
         topMenuButtonsController.setMainController(this);
+
+    }
+
+    public void setCenter(String fxmlPath) {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource(fxmlPath));
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
+        Parent parent = null;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        borderPane.setCenter(parent);
+
+    }
+
+    public void closeApplication() {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void setCaspian() {
+        Application.setUserAgentStylesheet(STYLESHEET_CASPIAN);
+    }
+
+    public void setModena() {
+        Application.setUserAgentStylesheet(STYLESHEET_MODENA);
+    }
+
+    public void about() {
+    }
+
+    public void setAlwaysOnTop(ActionEvent event) {
+        Stage stage = (Stage) borderPane.getScene().getWindow();
+        boolean value = ((CheckMenuItem) event.getSource()).selectedProperty().get();
+        stage.setAlwaysOnTop(value);
 
     }
 
